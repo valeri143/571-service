78ee468e2b590c701877deb32bfd4ce0223cb6f7
valeri143 <117216733+valeri143@users.noreply.github.com>
2023-09-22 20:22:04 +0000

    Deploying to gh-pages from @ valeri143/571-service@56c1bfa19cb249eb9813f59ed6e44eebc238af50 🚀

--- static/js/834.c325f40c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_571_service=self.webpackChunk_571_service||[]).push([[834],{6834:function(e,c,n){n.r(c);var r=n(184);c.default=function(){return(0,r.jsx)("h1",{children:"Hello"})}}}]);
-//# sourceMappingURL=834.c325f40c.chunk.js.map
