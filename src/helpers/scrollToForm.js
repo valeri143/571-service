071c43b378e74e577b9c10e235fd3d287b2a5ad1
valeri143 <valeria23200523251605@gmail.com>
2023-09-25 21:31:33 +0300

    set time out

--- src/helpers/scrollToForm.js
+++ src/helpers/scrollToForm.js
@@ -1,7 +1,17 @@
+// export const scrollToForm = () => {
+//     const form = document.getElementById('form');
+
+//     if (form) {
+//       form.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   };
+
 export const scrollToForm = () => {
+  setTimeout(() => {
     const form = document.getElementById('form');
 
     if (form) {
       form.scrollIntoView({ behavior: 'smooth' });
     }
-  };
+  }, 100); // Установите нужное вам значение задержки (в миллисекундах)
+};
