37551135ecb55f495024266a6c8283b709a66488
valeri143 <valeria23200523251605@gmail.com>
2023-09-26 13:53:17 +0300

    Update scrollToForm.js

--- src/helpers/scrollToForm.js
+++ src/helpers/scrollToForm.js
@@ -11,7 +11,7 @@ export const scrollToForm = () => {
     const form = document.getElementById('form');
 
     if (form) {
-      form.scrollIntoView({ behavior: 'smooth' });
+      form.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
     }
   }, 300); 
 };
