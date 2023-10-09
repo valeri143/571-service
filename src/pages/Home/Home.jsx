27fb64df544e06012b5cd4045a6f22db68938fa9
valeri143 <valeria23200523251605@gmail.com>
2023-10-09 13:08:28 +0300

    removed lazy pages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,27 @@
-import { lazy } from 'react';
+// import { lazy } from 'react';
 import { Hero } from 'components/Hero/Hero';
 import { SectionServices } from 'components/SectionServices/SectionServices';
+import SectionAbout from 'components/SectionAbout/SectionAbout';
+import SectionFeedbacks from 'components/SectionFeedbacks/SectionFeedbacks';
+import SectionCertifications from 'components/SectionCertifications/SectionCertifications';
 import SectionForm from 'components/SectionForm/SectionForm';
+import SectionBlog from 'components/SectionBlog/SectionBlog';
+import SectionContacts from 'components/SectionContacts/SectionContacts';
 
-const SectionAbout = lazy(() => import('components/SectionAbout/SectionAbout'));
-const SectionFeedbacks = lazy(() =>
-  import('components/SectionFeedbacks/SectionFeedbacks')
-);
-const SectionCertifications = lazy(() =>
-  import('components/SectionCertifications/SectionCertifications')
-);
+// const SectionAbout = lazy(() => import('components/SectionAbout/SectionAbout'));
+// const SectionFeedbacks = lazy(() =>
+//   import('components/SectionFeedbacks/SectionFeedbacks')
+// );
+// const SectionCertifications = lazy(() =>
+//   import('components/SectionCertifications/SectionCertifications')
+// );
 // const SectionForm = lazy(() => import('components/SectionForm/SectionForm'));
-const SectionBlog = lazy(() => import('components/SectionBlog/SectionBlog'));
-const SectionContacts = lazy(() =>
-  import('components//SectionContacts/SectionContacts')
-);
+// const SectionBlog = lazy(() => import('components/SectionBlog/SectionBlog'));
+// const SectionContacts = lazy(() =>
+//   import('components//SectionContacts/SectionContacts')
+// );
 
 const Home = () => {
-  // const formRef = useRef(null);
-
-  // const scrollToForm = () => {
-  //   if (formRef.current) {
-  //     formRef.current.scrollIntoView({
-  //       behavior: 'smooth',
-  //       block: 'center',
-  //       inline: 'nearest',
-  //     });
-  //   }
-  // };
-
   return (
     <>
       <Hero />
