841cc82625a097332c75f2675b31a9889351e98d
valeri143 <valeria23200523251605@gmail.com>
2023-10-09 15:36:49 +0300

    udated delay

--- src/helpers/scrollToForm.js
+++ src/helpers/scrollToForm.js
@@ -1,16 +1,8 @@
-// export const scrollToForm = () => {
-//     const form = document.getElementById('form');
-
-//     if (form) {
-//       form.scrollIntoView({ behavior: 'smooth' });
-//     }
-//   };
-
 export const scrollToForm = () => {
   const form = document.getElementById('form');
   setTimeout(() => {
     if (form) {
-      form.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+        form.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
     }
-  }, 1000); 
+  }, 500); 
 };
